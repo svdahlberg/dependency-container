010e0c7e4227677bb6001e9669bc1ad75ce6ada1
Svante Dahlberg <svante.dahlberg@bontouch.com>
2021-04-10 19:06:59 -0700

    Add dependency container + tests

--- Sources/DependencyContainer/DependencyContainer.swift
+++ Sources/DependencyContainer/DependencyContainer.swift
@@ -1,3 +1,26 @@
-struct DependencyContainer {
-    var text = "Hello, World!"
+class DependencyContainer {
+
+    typealias Dependency = Any
+    typealias Resolver = () -> Dependency
+    typealias Identifier = String
+
+    private var dependencies: [Identifier: Resolver]
+
+    public init() {
+        dependencies = [:]
+    }
+
+    public func register<Dependency>(_ dependency: Dependency.Type, resolver: @escaping Resolver) {
+        dependencies[resolverIdentifier(for: dependency)] = resolver
+    }
+
+    public func resolve<Dependency>(_ dependency: Dependency.Type) -> Dependency? {
+        let resolver = dependencies[resolverIdentifier(for: dependency)]
+        return resolver?() as? Dependency
+    }
+
+    private func resolverIdentifier<Dependency>(for dependency: Dependency.Type) -> Identifier {
+        String(describing: dependency)
+    }
+
 }
--- Tests/DependencyContainerTests/DependencyContainerTests.swift
+++ Tests/DependencyContainerTests/DependencyContainerTests.swift
@@ -1,15 +1,44 @@
 import XCTest
 @testable import DependencyContainer
 
-final class DependencyContainerTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(DependencyContainer().text, "Hello, World!")
+class DependencyContainerTests: XCTestCase {
+
+    var sut: DependencyContainer!
+
+    override func setUpWithError() throws {
+        sut = DependencyContainer()
+        super.setUp()
     }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    func testResolve_withRegisteredDependency_shouldInvokeResolver() {
+        // Given
+        sut.register(MockDependency.self) { MockDependency() }
+
+        // When
+        let mockDependency = sut.resolve(MockDependency.self)
+
+        // Then
+        XCTAssertEqual(mockDependency?.initInvokeCount, 1)
+    }
+
+    func testResolve_withNonRegisteredDependency_shouldReturnNil() {
+        // Given
+
+        // When
+        let mockDependency = sut.resolve(MockDependency.self)
+
+        // Then
+        XCTAssertNil(mockDependency)
+    }
+
+}
+
+struct MockDependency {
+
+    var initInvokeCount = 0
+
+    init() {
+        initInvokeCount += 1
+    }
+
 }
